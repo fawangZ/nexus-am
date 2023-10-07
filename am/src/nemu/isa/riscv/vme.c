67a69a499a97518702d71de50ddd1833911a220b
Fawang <fecheung@outlook.com>
2023-10-07 15:00:59 +0800

    add 1M address arch: xs

--- am/src/nemu/isa/riscv/vme.c
+++ am/src/nemu/isa/riscv/vme.c
@@ -12,7 +12,7 @@ static int vme_enable = 0;
 
 static const _Area segments[] = {      // Kernel memory mappings
 #if defined(__ARCH_RISCV64_NOOP) || defined(__ARCH_RISCV64_XS)
-  RANGE_LEN(0x80000000, 0x8000000), // PMEM
+  RANGE_LEN(0x100000000, 0x8000000), // PMEM
   RANGE_LEN(0x40600000, 0x1000),    // uart
   RANGE_LEN(CLINT_MMIO, 0x10000),   // clint/timer
   RANGE_LEN(FB_ADDR,    0x400000),  // vmem
